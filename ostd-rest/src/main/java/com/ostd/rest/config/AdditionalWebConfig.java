d54b460173c22f72ca0fa78f4b0fa934d69dc940
Sahak79 <sahak79@hotmail.com>
2018-02-17 18:00:02 +0400

    minor change in config

--- ostd-rest/src/main/java/com/ostd/rest/config/AdditionalWebConfig.java
+++ ostd-rest/src/main/java/com/ostd/rest/config/AdditionalWebConfig.java
@@ -1,11 +1,8 @@
 package com.ostd.rest.config;
 
-import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
-import org.springframework.boot.web.servlet.ErrorPage;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpStatus;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
